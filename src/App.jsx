3315fbebf9971f0921e3ca4590977f2877a74765
Osmium <aaroncayanan02@gmail.com>
2025-07-08 12:48:38 -0700

    create technology card

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,22 @@ import './App.css'
 import PortfolioCard from "./components/PortfolioCard.jsx";
 import AboutCard from "./components/AboutCard.jsx";
 import data from "./data.json";
+import TechCard from "./components/TechCard.jsx";
 
 function App() {
 
   return (
     <div>
-      <PortfolioCard jobHeader={data.portfolio["job-header"]} description={data.portfolio.description} location={data.portfolio.location}/>
-        <AboutCard content={data.about.content} />
+      <PortfolioCard
+          jobHeader={data.portfolio["job-header"]}
+          description={data.portfolio.description}
+          location={data.portfolio.location}
+      />
+      <AboutCard content={data.about.content} />
+      <TechCard
+          languages={data.technologies.languages}
+          frameworks={data.technologies.frameworks}
+      />
     </div>
   )
 }
---
+++ src/components/TechCard.jsx
@@ -0,0 +1,43 @@
+import PropTypes from 'prop-types';
+
+TechCard.propTypes = {
+    languages: PropTypes.arrayOf(PropTypes.string).isRequired,
+    frameworks: PropTypes.arrayOf(PropTypes.string).isRequired,
+};
+
+function TechCard(props) {
+    return (
+        <div className='gap-2.5'>
+            <div className='text-black text-xl font-bold'>
+                Current Technologies
+            </div>
+            <div>
+                <div className="text-black text-base font-semibold">
+                    Languages
+                </div>
+                <ul className='flex justify-start items-center gap-3.5'>
+                    {props.languages.map((language, index) => (
+                        <li key={index}
+                            className='justify-center text-yellow-800 text-sm font-normal px-1 py-0.5 bg-amber-300/50 rounded-lg inline-flex items-center'>
+                            {language}
+                        </li>
+                    ))}
+                </ul>
+            </div>
+            <div>
+                <div className="text-black text-base font-semibold">
+                Frameworks
+                </div>
+                <ul className='flex justify-start items-center gap-3.5'>
+                    {props.frameworks.map((framework, index) => (
+                        <li key={index} className='justify-center text-slate-500 text-sm font-normal px-1 py-0.5 bg-blue-400/50 rounded-lg inline-flex items-center'>
+                            {framework}
+                        </li>
+                    ))}
+                </ul>
+            </div>
+        </div>
+    );
+}
+
+export default TechCard;
