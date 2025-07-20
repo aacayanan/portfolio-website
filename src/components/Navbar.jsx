38b92f04e04562556d3ad628b94f2646dd9c0774
Aaron Cayanan <aaroncayanan02@gmail.com>
2025-07-20 01:56:49 -0700

    resume component on navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import { useLocation } from "react-router-dom";
+import {Link} from 'react-router-dom';
+import {useLocation} from "react-router-dom";
 
 function Navbar() {
     const location = useLocation();
@@ -14,20 +14,31 @@ function Navbar() {
     ];
 
     return (
-        <div className="flex justify-center items-center gap-5 h-12">
-            {navLinks.map((link) => (
-                <Link
-                    key={link.path}
-                    to={link.path}
-                    className={`${
-                        isActive === link.path
-                            ? "text-blue-500 underline underline-offset-4"
-                            : "text-gray-700 hover:text-black"
-                    }`}
-                >
-                    {link.name}
-                </Link>
-            ))}
+        <div className="flex justify-around items-center h-18 text-lg">
+            <div>
+                Aaron Cayanan
+            </div>
+            <div className='flex gap-5'>
+                {navLinks.map((link) => (
+                    <Link
+                        key={link.path}
+                        to={link.path}
+                        className={`${
+                            isActive === link.path
+                                ? "text-blue-500 underline underline-offset-4"
+                                : "text-gray-700 hover:text-black"
+                        }`}
+                    >
+                        {link.name}
+                    </Link>
+                ))}
+            </div>
+            <button
+                onClick={() => window.open("https://aaroncayanan.com/resume", "_blank")}
+                className='border-2 border-gray-700 rounded-md px-4 py-2 hover:bg-gray-700 hover:text-white transition-colors duration-300'
+            >
+                Resume
+            </button>
         </div>
     );
 
