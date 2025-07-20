4a5aa3c5a8620730883611848186f2c83430833a
Aaron Cayanan <aaroncayanan02@gmail.com>
2025-07-20 13:07:26 -0700

    add experience/education tab switcher and logic for handling

--- src/pages/Experience.jsx
+++ src/pages/Experience.jsx
@@ -1,15 +1,31 @@
-import React from "react";
+import React, {useState} from "react";
 import ExperienceCard from "../cards/ExperienceCard.jsx";
 import data from "../data.json";
-import ContactCard from "../cards/ContactCard.jsx";
 
 function Experience() {
+    const [activeTab, setActiveTab] = useState(true);
+
+    const tabs = [
+        { name: "Experience", value: true },
+        { name: "Education", value: false }
+    ]
+
     return (
-        <div className='flex py-24 px-12 justify-center'>
+        <div className='flex flex-col py-24 px-12 justify-center'>
+            <div className='flex w-full bg-gray-200 text-black text-lg font-semibold'>
+                {tabs.map((tab) => (
+                    <button
+                        key={tab.name}
+                        className={`flex w-full justify-center ${activeTab === tab.value ? 'bg-green-300' : 'bg-gray-400'}`}
+                        onClick={() => setActiveTab(tab.value)}
+                    >
+                        {tab.name}
+                    </button>
+                ))};
+            </div>
             <ExperienceCard content={data.experience}/>
         </div>
-    )
-        ;
+    );
 }
 
 export default Experience;
