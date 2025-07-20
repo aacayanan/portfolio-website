f9868006af9366468f555f908b1c61c89c6e9f45
Aaron Cayanan <aaroncayanan02@gmail.com>
2025-07-20 01:36:50 -0700

    add styling for active routes in nav

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,19 +1,22 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { useLocation } from "react-router-dom";
 
 function Navbar() {
+    const location = useLocation();
+    const isActive = location.pathname;
   return (
     <div className='flex justify-center items-center gap-5 h-12 shadow-md'>
-        <Link to={"/"}>
+        <Link to={"/"} className={`${isActive === "/" ? "text-blue-500" : "text-gray-700"}`}>
             Home
         </Link>
-        <Link to={"/experience"}>
+        <Link to={"/experience"} className={`${isActive === "/experience" ? "text-blue-500" : "text-gray-700"}`}>
             Experience
         </Link>
-        <Link to={"/skills"}>
+        <Link to={"/skills"} className={`${isActive === "/skills" ? "text-blue-500" : "text-gray-700"}`}>
             Skills
         </Link>
-        <Link to={"/projects"}>
+        <Link to={"/projects"} className={`${isActive === "/projects" ? "text-blue-500" : "text-gray-700"}`}>
             Projects
         </Link>
     </div>
