e98f0f1a3640efbcccc2e7ca3448c8db6aa59b20
Osmium <aaroncayanan02@gmail.com>
2025-08-15 13:44:03 -0700

    sliding bar experience tab

--- src/pages/Experience.jsx
+++ src/pages/Experience.jsx
@@ -13,22 +13,34 @@ function Experience() {
 
     return (
         <div className='flex flex-col max-w-4xl mx-auto py-10'>
-            <div className='flex w-full bg-gray-200 text-black text-lg font-semibold p-2 rounded-md'>
+            {/* Tab Switcher */}
+            <div className='relative flex w-full bg-gray-200 text-black text-lg font-semibold py-2 rounded-md'>
+                {/* Sliding Bar */}
+                <div
+                    className={`absolute top-2 left-2 w-[calc(50%-8px)] h-[calc(100%-16px)] bg-white shadow-md rounded-md transition-transform duration-300
+                    ${activeTab ? 'translate-x-0' : 'translate-x-full'}
+                `}/>
                 {tabs.map((tab) => (
                     <button
                         key={tab.name}
-                        className={`flex w-full justify-center text-gray-700' ${activeTab === tab.value ? 'bg-white shadow-md rounded-md text-black' : 'bg-gray-200 text-gray-600 hover:text-black'}`}
+                        className={`z-10 flex w-full justify-center text-gray-700 transition-all duration-300' 
+                        ${activeTab === tab.value 
+                            ? 'text-black' 
+                            : 'text-gray-600 hover:text-black'
+                        }`}
                         onClick={() => setActiveTab(tab.value)}
                     >
                         {tab.name}
                     </button>
                 ))}
             </div>
-            {activeTab ? (
-                <ExperienceCard content={data.experience} />
-            ): (
-                <EducationCard content={data.education} />
-            )}
+            <div className='transition-opacity duration-300'>
+                {activeTab ? (
+                    <ExperienceCard content={data.experience}/>
+                ) : (
+                    <EducationCard content={data.education}/>
+                )}
+            </div>
         </div>
     );
 }
