ac5d3c981dfcc8325222d6c7abb4f6cf1f627d3c
Osmium <aaroncayanan02@gmail.com>
2025-08-15 13:46:36 -0700

    fix education heading mislabel

--- src/cards/EducationCard.jsx
+++ src/cards/EducationCard.jsx
@@ -9,7 +9,7 @@ function ExperienceCard(props) {
     return (
         <div className='flex flex-col'>
             <div className='text-black text-xl font-bold py-2.5'>
-                Experience
+                Education
             </div>
             {props.content.map((experience, index) => (
                 <EducationContent content={experience} key={index}/>
