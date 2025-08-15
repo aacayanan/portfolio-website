c4fce59a18f0bba0eaaabdf167c810320e2631fd
Osmium <aaroncayanan02@gmail.com>
2025-08-15 13:08:59 -0700

    update hero card, update font to mono

--- src/cards/HeroCard.jsx
+++ src/cards/HeroCard.jsx
@@ -18,29 +18,29 @@ function HeroCard(props) {
     ]
 
     return (
-        <div className='flex gap-16 justify-center'>
+        <div className='flex gap-16 justify-center font-mono'>
             <div className='flex flex-col gap-8 justify-center'>
                 {/* Headline */}
                 <div className='flex flex-col gap-1'>
-                    <div className='text-7xl font-bold'>Aaron Justin Cayanan</div>
-                    <div className='text-6xl font-normal text-danger'>{`</${jobHeader}>`}</div>
-                    <div className='text-3xl font-normal text-gray-600'>based in {location} 📍</div>
+                    <div className='text-6xl font-semibold'>Aaron Justin Cayanan</div>
+                    <div className='text-5xl font-normal text-danger'>{`</${jobHeader}>`}</div>
+                    <div className='text-2xl font-normal text-gray-600'>based in {location}</div>
                 </div>
                 {/*  Benefits  */}
-                <div className='flex flex-wrap gap-8 text-lg font-medium text-gray-600'>
-                    {benefits.map((benefit, index) => (
-                        <div key={index} className='flex items-center gap-2'>
-                            <span className='text-xl'>{benefit.icon}</span>
-                            <span>{benefit.text}</span>
-                        </div>
-                    ))}
-                </div>
+                {/*<div className='flex flex-wrap gap-8 text-lg font-medium text-gray-600'>*/}
+                {/*    {benefits.map((benefit, index) => (*/}
+                {/*        <div key={index} className='flex items-center gap-2'>*/}
+                {/*            <span className='text-xl'>{benefit.icon}</span>*/}
+                {/*            <span>{benefit.text}</span>*/}
+                {/*        </div>*/}
+                {/*    ))}*/}
+                {/*</div>*/}
                 {/* Description */}
                 <div className='text-gray-500 max-w-xl text-base leading-relaxed'>
                     {description}
                 </div>
             </div>
-            <img src={profilePicture} alt="Profile" className='w-2xl h-2xl rounded-2xl p-2'/>
+            <img src={profilePicture} alt="Profile" className='w-xl h-xl rounded-2xl p-2 object-contain'/>
         </div>
     );
 }
